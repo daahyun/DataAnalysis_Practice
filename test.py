cd52c987a604c252925823afe2777b621d3246a3
daahyun <75472907+daahyun@users.noreply.github.com>
2021-05-03 17:09:37 +0900

    test_graph
    데이터 불러와서 그래프 그리기

---
+++ test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df1 = pd.read_excel('~/Documents/GitHub/DataAnalysis_Practice/pfb.xlsx') 
+print(df1)
+
+fig, axes = plt.subplots(1, 1) 
+plt.plot(df1['date'], df1['sl'])
+plt.xlabel('Date')
+plt.ylabel('Stress level')
+plt.show()
+
+#시계열 데이터 추출
+#두개 선 그래프 그리기
